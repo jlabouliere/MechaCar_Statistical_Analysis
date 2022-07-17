5b083585e6a352fb8bc1f528f7e6ca50413fcad3
jlabouliere <98665941+jlabouliere@users.noreply.github.com>
2022-07-16 21:57:06 -0500

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -7,7 +7,7 @@ MechaCar_mpg <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F)
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,data=MechaCar_mpg)
 #determine the p-value and r-squared value
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,data=MechaCar_mpg)) 
-
+model
 #Deliverable 2
 # import and read the csv file as a table
 Suspension_Coil <- read.csv('Suspension_Coil.csv',stringsAsFactors = F)
@@ -18,3 +18,11 @@ head(total_summary)
 #create a lot_summary datafrome w/group_by and summarize()
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(MeanPSI=mean(PSI), MedianPSI=median(PSI), VarPSI=var(PSI), SDPSI=sd(PSI),.groups = 'keep')
 head(lot_summary)
+
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))              
